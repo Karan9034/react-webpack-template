34dbfef8b99d2593e759a3c17c7d3a58d0df7b66
Karan9034 <karan.agr9034@gmail.com>
2022-04-16 09:35:57 +0530

    Boilerplate updated

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import img from '../public/img/img.png'
+
+const App = () => {
+    return (
+        <>
+            <h1>Hello</h1>
+            <img src={img}></img>
+        </>
+    )
+}
+
+export default App
